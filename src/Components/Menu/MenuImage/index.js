2fb7df05419121ab460c4fc78bca79238c9f11ba
William Gehrke <williamegehrke@gmail.com>
2022-12-05 11:07:43 -0500

    added menu in carousel format

--- src/Components/Menu/MenuImage/index.js
+++ src/Components/Menu/MenuImage/index.js
@@ -1,12 +1,41 @@
 import React from "react";
 import Menu1 from '../../../Images/MenuPicOne.png'
 import Menu2 from '../../../Images/MenuPicTwo.png'
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
+
+const responsive = {
+  superLargeDesktop: {
+    // the naming can be any, depends on you.
+    breakpoint: { max: 4000, min: 3000 },
+    items: 5
+  },
+  desktop: {
+    breakpoint: { max: 3000, min: 1024 },
+    items: 3
+  },
+  tablet: {
+    breakpoint: { max: 1024, min: 464 },
+    items: 2
+  },
+  mobile: {
+    breakpoint: { max: 464, min: 0 },
+    items: 1
+  }
+};
 
 function MenuImage() {
     return (
  <div className="map" align="center">
-        <img className="mapImage" src={Menu1} alt="MenuImage" />
-        <img className="mapImage" src={Menu2} alt="MenuImage" />
+  <Carousel responsive={responsive}>
+  <img className="mapImage" src={Menu1} alt="MenuImage" />
+  <div><br></br></div>
+  <div><br></br></div>
+  <img className="mapImage" src={Menu2} alt="MenuImage" />
+  <div><br></br></div>
+  <div><br></br></div>
+ 
+</Carousel>
       </div>
       
     );
